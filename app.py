968905fe0067ce780c51e9061958d885a5b945ce
Yazan <yazan@example.com>
2025-12-12 18:52:46 +0100

    erste version meiner webseite

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def startseite():
+   return "Hallo Quzi, ich bin Yazan und das ist meine erste Internetseite habibiii"
+
+if __name__ == "__main__":
+    app.run(debug=True)
