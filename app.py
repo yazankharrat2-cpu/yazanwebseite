daa6741692b2434968b90b18afd86ca41e6d9c5d
Yazan <yazan@example.com>
2025-12-12 19:22:39 +0100

    Text geändert

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def startseite():
-   return "Hallo Milano , ich bin Yazan und das ist meine erste Internetseite habibiii"
+   return "Hallo Herr Saadieh , ich bin Yazan und das ist meine erste Internetseite habibiii"
 
 if __name__ == "__main__":
     app.run(debug=True)
