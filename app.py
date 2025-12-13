7e11c5dac98e106e46d88e8baf3d1b56e2fdf40c
Yazan <yazan@example.com>
2025-12-13 14:12:45 +0100

    HTML Seite hinzugefügt

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def startseite():
-   return "Hallo Herr Saadieh , ich bin Yazan und das ist meine erste Internetseite die ich am 12.12.2025 erstellt habe :) "
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
